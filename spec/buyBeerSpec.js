d666ee97a5144fb6f8f4516f1211533abd1dc6ab
susantikcm <susantikcm@yahoo.com>
2020-09-29 17:16:36 +1000

    Submit testing exercise 1-3

--- spec/buyBeerSpec.js
+++ spec/buyBeerSpec.js
@@ -1,28 +1,29 @@
 describe('When can Buy Beer', () => {
-    const age18Above = 18;
+    const age18Above = 19;
     const ageUnder18 = 15;
+    const gregAge = greg.age;
 
     describe('When age is 18', () => {
         it(`Greg is ${age18Above}, he can buy beer!`, () => {
-            const gregAge = greg.age;
             greg.age = age18Above;
 
             const result = canGregBuyBeer();
 
             expect(result).toBe(`Greg is ${greg.age}, he can buy beer!`);
-            greg.age = gregAge;
         });
     });
 
     describe('When age is under 18', () => {
         it(`Greg is ${ageUnder18}, he can't buy beer :(`, () => {
-            const gregAge = greg.age;
             greg.age = ageUnder18;
 
             const result = canGregBuyBeer();
 
             expect(result).toBe(`Greg is ${greg.age}, he can't buy beer :(`);
-            greg.age = gregAge;
         });
+    });
+
+    afterEach(() => {
+        greg.age = gregAge;
     });
 });
